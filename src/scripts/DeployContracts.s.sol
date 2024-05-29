d072468c4a7bda0026bb37eab50f8e880dd46f19
SupaMega24 <138974329+SupaMega24@users.noreply.github.com>
2024-05-30 02:14:14 +0900

    Create Chainlink Function Contract

--- src/scripts/DeployContracts.s.sol
+++ src/scripts/DeployContracts.s.sol
@@ -10,15 +10,23 @@ import {Interaction} from "../FrontendInteraction.sol";
 
 interface IWETH {
     function deposit() external payable;
+
     function transfer(address to, uint value) external returns (bool);
+
     function balanceOf(address s_owner) external view returns (uint);
-    function allowance(address s_owner, address spender) external view returns (uint);
+
+    function allowance(
+        address s_owner,
+        address spender
+    ) external view returns (uint);
+
     function approve(address spender, uint amount) external returns (bool);
 }
 
 contract DeployContracts is Script {
- // address public constant wethAddress = 0x7b79995e5f793A07Bc00c21412e50Ecae098E7f9; // On Sepolia
-    address public constant wethAddress = 0x52eF3d68BaB452a294342DC3e5f464d7f610f72E; // On Amoy
+    // address public constant wethAddress = 0x7b79995e5f793A07Bc00c21412e50Ecae098E7f9; // On Sepolia
+    address public constant wethAddress =
+        0x52eF3d68BaB452a294342DC3e5f464d7f610f72E; // On Amoy
     uint256 public initialSupply = 1000000 * 10 ** 18;
     uint256 public initial_rbTokens_inPool = 10000 * 10 ** 18;
     uint256 public initial_weth_inPool = 10000 * 10 ** 18;
@@ -40,8 +48,14 @@ contract DeployContracts is Script {
 
         pool.setIntialBalanceOfpool(initial_rbTokens_inPool);
 
-        console.log("Amount of WETH in Pool", IWETH(wethAddress).balanceOf(address(pool)));
-        console.log("Amount of RBToken in Pool", rbToken.balanceOf(address(pool)));
+        console.log(
+            "Amount of WETH in Pool",
+            IWETH(wethAddress).balanceOf(address(pool))
+        );
+        console.log(
+            "Amount of RBToken in Pool",
+            rbToken.balanceOf(address(pool))
+        );
 
         MarketPlace marketPlace = new MarketPlace(
             payable(address(pool)),
@@ -55,13 +69,12 @@ contract DeployContracts is Script {
         // Interaction interaction = new Interaction(address(reward), address(pool), address(rbToken), address(marketPlace));
 
         vm.stopBroadcast();
-        
+
         console.log("RB Token Address", address(rbToken));
         console.log("Pool Address", address(pool));
         console.log("Marketplace Address", address(marketPlace));
         console.log("Reward Address", address(reward));
         // console.log("Interaction Address", address(interaction));
-      
     }
 }
 
@@ -125,7 +138,6 @@ contract DeployContracts is Script {
 //         console.log("Reward Address", address(reward));
 //     }
 // }
-
 
 /**
  * @dev The code below needs to be sorted.
